68773f3728f445bda0e5ce4a8e24b058b3481572
PlatonNovikov <kanal.platon@gmail.com>
2026-04-29 17:46:51 +0200

    still making stuff wow

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #include "vector.h"
 
@@ -8,12 +9,12 @@
 
 typedef struct
 {
-	__uint32_t	width;
-	__uint32_t	height;
-	
+	uint32_t	width;
+	uint32_t	height;
+
 	// considering that the player spawns at (0, 0), this is needed to normalise coordinates
-	__uint32_t player_offset_x;
-	__uint32_t player_offset_y;
+	int16_t player_offset_x;
+	int16_t player_offset_y;
 
 	char	name[8];
 	t_vec	*vertex; // (vertex_t *)
@@ -28,37 +29,39 @@ typedef struct
 	t_vec	*things;
 	t_vec	*reject;
 	t_vec	*blockmap;
+
+	FILE	*file;
 } map_t;
 
 typedef struct
 {
-	__uint8_t	name[4]; //IWAD or PWAD
-	__uint32_t	entries; //The number entries in the directory
-	__uint32_t	offset; //Offset in bytes to the directory in the WAD file.
+	uint8_t	name[4]; //IWAD or PWAD
+	uint32_t	entries; //The number entries in the directory
+	uint32_t	offset; //Offset in bytes to the directory in the WAD file.
 } wad_header_t;
 
 typedef struct
 {
-	__int16_t	x; //X Position
-	__int16_t	y; //Y Position
+	int16_t	x; //X Position
+	int16_t	y; //Y Position
 } vertex_t;
 
 typedef struct
 {
-	__uint16_t	start; //Start vertex
-	__uint16_t	end; //End vertex
-	__uint16_t	flags;
-	__uint16_t	type; //Line type / Action
-	__uint16_t	sector_tag; //Sector tag
-	__uint16_t	r_sidedef; //Right sidedef ( 0xFFFF side not present )
-	__uint16_t	l_sidedef; //Left sidedef ( 0xFFFF side not present )
+	uint16_t	start; //Start vertex
+	uint16_t	end; //End vertex
+	uint16_t	flags;
+	uint16_t	type; //Line type / Action
+	uint16_t	sector_tag; //Sector tag
+	uint16_t	r_sidedef; //Right sidedef ( 0xFFFF side not present )
+	uint16_t	l_sidedef; //Left sidedef ( 0xFFFF side not present )
 } linedef_t;
 
 typedef struct
 {
-	__uint32_t	filepos; // An integer holding a pointer to the start of the lump's data in the file.
-	__uint32_t	size; // An integer representing the size of the lump in bytes.
-	__uint8_t	name[8];
+	uint32_t	filepos; // An integer holding a pointer to the start of the lump's data in the file.
+	uint32_t	size; // An integer representing the size of the lump in bytes.
+	uint8_t	name[8];
 } filelump_t;
 
 // Linedef Flags Values
@@ -77,60 +80,60 @@ typedef struct
 
 typedef struct
 {
-    __int16_t	x_offset;
-    __int16_t	y_offset;
-    __int8_t	upper_texture[8];
-    __int8_t	lower_texture[8];
-    __int8_t	middle_texture[8];
-    __uint16_t	sector_id;
+    int16_t	x_offset;
+    int16_t	y_offset;
+    int8_t	upper_texture[8];
+    int8_t	lower_texture[8];
+    int8_t	middle_texture[8];
+    uint16_t	sector_id;
 } sideddef_t;
 
 typedef struct
 {
-    __int16_t	floor_height;
-    __int16_t	ceiling_height;
-    __int8_t	floor_texture[8];
-    __int8_t	ceiling_texture[8];
-    __uint16_t	light_level;
-    __uint16_t	type;
-    __uint16_t	tag;
+    int16_t	floor_height;
+    int16_t	ceiling_height;
+    int8_t	floor_texture[8];
+    int8_t	ceiling_texture[8];
+    uint16_t	light_level;
+    uint16_t	type;
+    uint16_t	tag;
 } sector_t;
 
 typedef struct
 {
-	__uint16_t	seg_count; //The number of segs that compose this sub-segment
-	__uint16_t	seg_start; //The index of the first seg in the seg list
+	uint16_t	seg_count; //The number of segs that compose this sub-segment
+	uint16_t	seg_start; //The index of the first seg in the seg list
 } ssector_t;
 
 typedef struct
 {
-	__uint16_t	start; //Start vertex
-	__uint16_t	end; //End vertex
-	__uint16_t	angle;
-	__uint16_t	linedef; //Linedef index
-	__uint16_t	direction; //0 (same as linedef) or 1 (opposite of linedef)
-	__uint16_t	offset; //distance along linedef to start of seg
+	uint16_t	start; //Start vertex
+	uint16_t	end; //End vertex
+	uint16_t	angle;
+	uint16_t	linedef; //Linedef index
+	uint16_t	direction; //0 (same as linedef) or 1 (opposite of linedef)
+	uint16_t	offset; //distance along linedef to start of seg
 } seg_t;
 
 typedef struct
 {
-	__int16_t x_partition; //X coordinate of the splitter
-	__int16_t y_partition; //Y coordinate of the splitter
-	__int16_t change_x_partition; //The amount to move in X to reach end of splitter
-	__int16_t change_y_partition; //The amount to move in y to reach end of splitter
+	int16_t x_partition; //X coordinate of the splitter
+	int16_t y_partition; //Y coordinate of the splitter
+	int16_t change_x_partition; //The amount to move in X to reach end of splitter
+	int16_t change_y_partition; //The amount to move in y to reach end of splitter
 
-	__int16_t right_box_top; //First corner of right box (Y coordinate)
-	__int16_t right_box_bottom; //Second corner of right box (Y coordinate)
-	__int16_t right_box_left; //First corner of right box (X coordinate)
-	__int16_t right_box_right; //Second corner of right box (X coordinate)
+	int16_t right_box_top; //First corner of right box (Y coordinate)
+	int16_t right_box_bottom; //Second corner of right box (Y coordinate)
+	int16_t right_box_left; //First corner of right box (X coordinate)
+	int16_t right_box_right; //Second corner of right box (X coordinate)
 
-	__int16_t left_box_top; //First corner of left box (Y coordinate)
-	__int16_t left_box_bottom; //Second corner of left box (Y coordinate)
-	__int16_t left_box_left; //First corner of left box (X coordinate)
-	__int16_t left_box_right; //Second corner of left box (X coordinate)
+	int16_t left_box_top; //First corner of left box (Y coordinate)
+	int16_t left_box_bottom; //Second corner of left box (Y coordinate)
+	int16_t left_box_left; //First corner of left box (X coordinate)
+	int16_t left_box_right; //Second corner of left box (X coordinate)
 
-	__uint16_t right_child_id; //Index of the right child + sub-sector indicator
-	__uint16_t left_child_id; //Index of the left child + sub-sector indicator
+	uint16_t right_child_id; //Index of the right child + sub-sector indicator
+	uint16_t left_child_id; //Index of the left child + sub-sector indicator
 } node_t;
 
 enum EMAPLUMPSINDEX
@@ -164,9 +167,33 @@ void	map_init(map_t	*map)
 	map->blockmap = vec_init();
 }
 
+void	write16rev(FILE *file, int16_t v)
+{
+	uint8_t	tmp;
+
+	tmp = v & 255;
+	fwrite(&tmp, sizeof(int8_t), 1, file);
+	tmp = (v >> 8) & 255;
+	fwrite(&tmp, sizeof(int8_t), 1, file);
+}
+
+void	write32rev(FILE *file, int32_t v)
+{
+	uint8_t	tmp;
+
+	tmp = v & 255;
+	fwrite(&tmp, sizeof(int8_t), 1, file);
+	tmp = (v >> 8) & 255;
+	fwrite(&tmp, sizeof(int8_t), 1, file);
+	tmp = (v >> 16) & 255;
+	fwrite(&tmp, sizeof(int8_t), 1, file);
+	tmp = (v >>24) & 255;
+	fwrite(&tmp, sizeof(int8_t), 1, file);
+}
+
 //returns tag of a vertex
 //given vertex SHOULD exist
-__uint16_t	find_vertex(map_t *map, __uint16_t x, __uint16_t y)
+uint16_t	find_vertex(map_t *map, int16_t x, int16_t y)
 {
 	for (size_t	i = 0; i < map->vertex->size; i++)
 	{
@@ -178,7 +205,7 @@ __uint16_t	find_vertex(map_t *map, __uint16_t x, __uint16_t y)
 }
 
 //checks if vertex exist
-bool	check_vertex(map_t *map, __uint16_t x, __uint16_t y)
+bool	check_vertex(map_t *map, int16_t x, int16_t y)
 {
 	for (size_t	i = 0; i < map->vertex->size; i++)
 	{
@@ -189,7 +216,7 @@ bool	check_vertex(map_t *map, __uint16_t x, __uint16_t y)
 	return (false);
 }
 
-__uint16_t	make_vertex(map_t *map, __uint16_t x, __uint16_t y)
+uint16_t	make_vertex(map_t *map, int16_t x, int16_t y)
 {
 	vertex_t	*v;
 
@@ -203,7 +230,7 @@ __uint16_t	make_vertex(map_t *map, __uint16_t x, __uint16_t y)
 	return (map->vertex->size - 1);
 }
 
-void	make_linedef(map_t *map, __uint16_t x1, __uint16_t y1, __int16_t x2, __uint16_t y2)
+void	make_linedef(map_t *map, int16_t x1, int16_t y1, int16_t x2, int16_t y2)
 {
 	linedef_t *l = calloc(1, sizeof(linedef_t));
 
@@ -215,9 +242,10 @@ void	make_linedef(map_t *map, __uint16_t x1, __uint16_t y1, __int16_t x2, __uint
 	vec_append(map->linedef, l);
 }
 
-__uint32_t	calc_dir_offset(map_t *map)
+uint32_t	dir_offset(map_t *map)
 {
 	return (
+		HEADER_OFF +
 		map->vertex->size * sizeof(vertex_t) +
 		map->linedef->size * sizeof(linedef_t) +
 		map->sideddefs->size * sizeof(sideddef_t) +
@@ -227,18 +255,45 @@ __uint32_t	calc_dir_offset(map_t *map)
 		map->nodes->size * sizeof(node_t));
 }
 
+uint32_t	linedef_offset(map_t *map)
+{
+	return (
+		HEADER_OFF +
+		map->vertex->size * sizeof(vertex_t)
+	);
+}
+
+void write_dir(map_t *map)
+{
+	fseek(map->file, dir_offset(map), SEEK_SET);
+}
+
 void write_stuff(map_t *map)
 {
-	__uint32_t	offset = HEADER_OFF;
-	
+	uint32_t	offset = HEADER_OFF;
+
+	fseek(map->file, HEADER_OFF, SEEK_SET);
 	for (int i = 0; i < map->vertex->size; i++)
 	{
-
+		fwrite(vec_get(map->vertex, i), sizeof(vertex_t), 1, map->file);
 	}
+}
+
+void create_square(map_t *map, int16_t x1, int16_t y1, int16_t x2, int16_t y2)
+{
+	make_linedef(map, x1, y1, x1, y2);
+	make_linedef(map, x1, y1, x2, y1);
+	make_linedef(map, x2, y1, x2, y2);
+	make_linedef(map, x1, y2, x2, y2);
 }
 
 int main()
 {
+	map_t map = {0};
+	map.file = fopen("map.wad", "r+b");
+
+	create_square(&map, 10, 10, 20, 20);
+	write_stuff(&map);
 	return (0);
 }
 
